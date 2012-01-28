2261182e09e226f4dd9ff52435c1c66d5005f265
Matthias Georgi <matti.georgi@gmail.com>
2012-01-28 18:14:38 +0100

    init

---
+++ Classes/aurioTouchAppDelegate.mm
@@ -0,0 +1,485 @@
+/*
+
+    File: aurioTouchAppDelegate.mm
+Abstract: n/a
+ Version: 1.21
+
+Disclaimer: IMPORTANT:  This Apple software is supplied to you by Apple
+Inc. ("Apple") in consideration of your agreement to the following
+terms, and your use, installation, modification or redistribution of
+this Apple software constitutes acceptance of these terms.  If you do
+not agree with these terms, please do not use, install, modify or
+redistribute this Apple software.
+
+In consideration of your agreement to abide by the following terms, and
+subject to these terms, Apple grants you a personal, non-exclusive
+license, under Apple's copyrights in this original Apple software (the
+"Apple Software"), to use, reproduce, modify and redistribute the Apple
+Software, with or without modifications, in source and/or binary forms;
+provided that if you redistribute the Apple Software in its entirety and
+without modifications, you must retain this notice and the following
+text and disclaimers in all such redistributions of the Apple Software.
+Neither the name, trademarks, service marks or logos of Apple Inc. may
+be used to endorse or promote products derived from the Apple Software
+without specific prior written permission from Apple.  Except as
+expressly stated in this notice, no other rights or licenses, express or
+implied, are granted by Apple herein, including but not limited to any
+patent rights that may be infringed by your derivative works or by other
+works in which the Apple Software may be incorporated.
+
+The Apple Software is provided by Apple on an "AS IS" basis.  APPLE
+MAKES NO WARRANTIES, EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION
+THE IMPLIED WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY AND FITNESS
+FOR A PARTICULAR PURPOSE, REGARDING THE APPLE SOFTWARE OR ITS USE AND
+OPERATION ALONE OR IN COMBINATION WITH YOUR PRODUCTS.
+
+IN NO EVENT SHALL APPLE BE LIABLE FOR ANY SPECIAL, INDIRECT, INCIDENTAL
+OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+INTERRUPTION) ARISING IN ANY WAY OUT OF THE USE, REPRODUCTION,
+MODIFICATION AND/OR DISTRIBUTION OF THE APPLE SOFTWARE, HOWEVER CAUSED
+AND WHETHER UNDER THEORY OF CONTRACT, TORT (INCLUDING NEGLIGENCE),
+STRICT LIABILITY OR OTHERWISE, EVEN IF APPLE HAS BEEN ADVISED OF THE
+POSSIBILITY OF SUCH DAMAGE.
+
+Copyright (C) 2010 Apple Inc. All Rights Reserved.
+
+
+*/
+
+#import "aurioTouchAppDelegate.h"
+#import "AudioToolbox/AudioToolbox.h"
+#import "CAXException.h"
+
+@implementation aurioTouchAppDelegate
+
+// value, a, r, g, b
+GLfloat colorLevels[] = {
+0., 1., 0., 0., 0., 
+.333, 1., .7, 0., 0., 
+.667, 1., 0., 0., 1., 
+1., 1., 0., 1., 1., 
+};
+
+@synthesize window;
+@synthesize view;
+
+@synthesize rioUnit;
+@synthesize unitIsRunning;
+@synthesize unitHasBeenCreated;
+@synthesize inputProc;
+
+#pragma mark-
+
+void cycleOscilloscopeLines()
+{
+	// Cycle the lines in our draw buffer so that they age and fade. The oldest line is discarded.
+	int drawBuffer_i;
+	for (drawBuffer_i=(kNumDrawBuffers - 2); drawBuffer_i>=0; drawBuffer_i--)
+		memmove(drawBuffers[drawBuffer_i + 1], drawBuffers[drawBuffer_i], drawBufferLen);
+}
+
+#pragma mark -Audio Session Interruption Listener
+
+void rioInterruptionListener(void *inClientData, UInt32 inInterruption)
+{
+	printf("Session interrupted! --- %s ---", inInterruption == kAudioSessionBeginInterruption ? "Begin Interruption" : "End Interruption");
+	
+	aurioTouchAppDelegate *THIS = (aurioTouchAppDelegate*)inClientData;
+	
+	if (inInterruption == kAudioSessionEndInterruption) {
+		// make sure we are again the active session
+		XThrowIfError(AudioSessionSetActive(true), "couldn't set audio session active");
+		XThrowIfError(AudioOutputUnitStart(THIS->rioUnit), "couldn't start unit");
+	}
+	
+	if (inInterruption == kAudioSessionBeginInterruption) {
+		XThrowIfError(AudioOutputUnitStop(THIS->rioUnit), "couldn't stop unit");
+    }
+}
+
+#pragma mark -Audio Session Property Listener
+
+void propListener(	void *                  inClientData,
+					AudioSessionPropertyID	inID,
+					UInt32                  inDataSize,
+					const void *            inData)
+{
+	aurioTouchAppDelegate *THIS = (aurioTouchAppDelegate*)inClientData;
+	if (inID == kAudioSessionProperty_AudioRouteChange)
+	{
+		try {
+			 UInt32 isAudioInputAvailable; 
+			 UInt32 size = sizeof(isAudioInputAvailable);
+			 XThrowIfError(AudioSessionGetProperty(kAudioSessionProperty_AudioInputAvailable, &size, &isAudioInputAvailable), "couldn't get AudioSession AudioInputAvailable property value");
+			 
+			 if(THIS->unitIsRunning && !isAudioInputAvailable)
+			 {
+				 XThrowIfError(AudioOutputUnitStop(THIS->rioUnit), "couldn't stop unit");
+				 THIS->unitIsRunning = false;
+			 }
+			 
+			 else if(!THIS->unitIsRunning && isAudioInputAvailable)
+			 {
+				 XThrowIfError(AudioSessionSetActive(true), "couldn't set audio session active\n");
+			 
+				 if (!THIS->unitHasBeenCreated)	// the rio unit is being created for the first time
+				 {
+					 XThrowIfError(SetupRemoteIO(THIS->rioUnit, THIS->inputProc, THIS->thruFormat), "couldn't setup remote i/o unit");
+					 THIS->unitHasBeenCreated = true;
+					 
+					 THIS->dcFilter = new DCRejectionFilter[THIS->thruFormat.NumberChannels()];
+					 
+					 UInt32 maxFPS;
+					 size = sizeof(maxFPS);
+					 XThrowIfError(AudioUnitGetProperty(THIS->rioUnit, kAudioUnitProperty_MaximumFramesPerSlice, kAudioUnitScope_Global, 0, &maxFPS, &size), "couldn't get the remote I/O unit's max frames per slice");
+					 
+					 THIS->oscilLine = (GLfloat*)malloc(drawBufferLen * 2 * sizeof(GLfloat));
+				 }
+				 
+				 XThrowIfError(AudioOutputUnitStart(THIS->rioUnit), "couldn't start unit");
+				 THIS->unitIsRunning = true;
+			 }
+						
+		} catch (CAXException e) {
+			char buf[256];
+			fprintf(stderr, "Error: %s (%s)\n", e.mOperation, e.FormatError(buf));
+		}
+		
+	}
+}
+
+#pragma mark -RIO Render Callback
+
+static OSStatus	PerformThru(
+							void						*inRefCon, 
+							AudioUnitRenderActionFlags 	*ioActionFlags, 
+							const AudioTimeStamp 		*inTimeStamp, 
+							UInt32 						inBusNumber, 
+							UInt32 						inNumberFrames, 
+							AudioBufferList 			*ioData)
+{
+	aurioTouchAppDelegate *THIS = (aurioTouchAppDelegate *)inRefCon;
+	OSStatus err = AudioUnitRender(THIS->rioUnit, ioActionFlags, inTimeStamp, 1, inNumberFrames, ioData);
+	if (err) { printf("PerformThru: error %d\n", (int)err); return err; }
+	
+	// Remove DC component
+	for(UInt32 i = 0; i < ioData->mNumberBuffers; ++i)
+		THIS->dcFilter[i].InplaceFilter((SInt32*)(ioData->mBuffers[i].mData), inNumberFrames, 1);
+	
+	
+    // The draw buffer is used to hold a copy of the most recent PCM data to be drawn on the oscilloscope
+    if (drawBufferLen != drawBufferLen_alloced)
+    {
+        int drawBuffer_i;
+        
+        // Allocate our draw buffer if needed
+        if (drawBufferLen_alloced == 0)
+            for (drawBuffer_i=0; drawBuffer_i<kNumDrawBuffers; drawBuffer_i++)
+                drawBuffers[drawBuffer_i] = NULL;
+        
+        // Fill the first element in the draw buffer with PCM data
+        for (drawBuffer_i=0; drawBuffer_i<kNumDrawBuffers; drawBuffer_i++)
+        {
+            drawBuffers[drawBuffer_i] = (SInt8 *)realloc(drawBuffers[drawBuffer_i], drawBufferLen);
+            bzero(drawBuffers[drawBuffer_i], drawBufferLen);
+        }
+        
+        drawBufferLen_alloced = drawBufferLen;
+    }
+    
+    int i;
+    
+    SInt8 *data_ptr = (SInt8 *)(ioData->mBuffers[0].mData);
+    for (i=0; i<inNumberFrames; i++)
+    {
+        if ((i+drawBufferIdx) >= drawBufferLen)
+        {
+            // cycleOscilloscopeLines();
+            drawBufferIdx = -i;
+        }
+        drawBuffers[0][i + drawBufferIdx] = data_ptr[2];
+        data_ptr += 4;
+    }
+    drawBufferIdx += inNumberFrames;
+	
+	return err;
+}
+
+#pragma mark-
+
+- (void)applicationDidFinishLaunching:(UIApplication *)application
+{	
+	// Turn off the idle timer, since this app doesn't rely on constant touch input
+	application.idleTimerDisabled = YES;
+		
+	// Initialize our remote i/o unit
+	
+	inputProc.inputProc = PerformThru;
+	inputProc.inputProcRefCon = self;
+
+	try {	
+		
+		// Initialize and configure the audio session
+		XThrowIfError(AudioSessionInitialize(NULL, NULL, rioInterruptionListener, self), "couldn't initialize audio session");
+			
+		UInt32 audioCategory = kAudioSessionCategory_PlayAndRecord;
+		XThrowIfError(AudioSessionSetProperty(kAudioSessionProperty_AudioCategory, sizeof(audioCategory), &audioCategory), "couldn't set audio category");
+		XThrowIfError(AudioSessionAddPropertyListener(kAudioSessionProperty_AudioRouteChange, propListener, self), "couldn't set property listener");
+
+		Float32 preferredBufferSize = .005;
+		XThrowIfError(AudioSessionSetProperty(kAudioSessionProperty_PreferredHardwareIOBufferDuration, sizeof(preferredBufferSize), &preferredBufferSize), "couldn't set i/o buffer duration");
+		
+		UInt32 size = sizeof(hwSampleRate);
+		XThrowIfError(AudioSessionGetProperty(kAudioSessionProperty_CurrentHardwareSampleRate, &size, &hwSampleRate), "couldn't get hw sample rate");
+		
+		XThrowIfError(AudioSessionSetActive(true), "couldn't set audio session active\n");
+
+		XThrowIfError(SetupRemoteIO(rioUnit, inputProc, thruFormat), "couldn't setup remote i/o unit");
+		unitHasBeenCreated = true;
+		
+		dcFilter = new DCRejectionFilter[thruFormat.NumberChannels()];
+
+		UInt32 maxFPS;
+		size = sizeof(maxFPS);
+		XThrowIfError(AudioUnitGetProperty(rioUnit, kAudioUnitProperty_MaximumFramesPerSlice, kAudioUnitScope_Global, 0, &maxFPS, &size), "couldn't get the remote I/O unit's max frames per slice");
+		
+		oscilLine = (GLfloat*)malloc(drawBufferLen * 2 * sizeof(GLfloat));
+
+		XThrowIfError(AudioOutputUnitStart(rioUnit), "couldn't start remote i/o unit");
+
+		size = sizeof(thruFormat);
+		XThrowIfError(AudioUnitGetProperty(rioUnit, kAudioUnitProperty_StreamFormat, kAudioUnitScope_Output, 1, &thruFormat, &size), "couldn't get the remote I/O unit's output client format");
+		
+		unitIsRunning = 1;
+	}
+	catch (CAXException &e) {
+		char buf[256];
+		fprintf(stderr, "Error: %s (%s)\n", e.mOperation, e.FormatError(buf));
+		unitIsRunning = 0;
+		if (dcFilter) delete[] dcFilter;
+	}
+	catch (...) {
+		fprintf(stderr, "An unknown error occurred\n");
+		unitIsRunning = 0;
+		if (dcFilter) delete[] dcFilter;
+	}
+	
+	// Set ourself as the delegate for the EAGLView so that we get drawing and touch events
+	view.delegate = self;
+	
+	// Enable multi touch so we can handle pinch and zoom in the oscilloscope
+	view.multipleTouchEnabled = YES;
+
+	// Set up the view to refresh at 20 hz
+	[view setAnimationInterval:1./20.];
+	[view startAnimation];
+}
+
+- (void)applicationDidBecomeActive:(UIApplication *)application {
+	//start animation now that we're in the foreground
+    view.applicationResignedActive = NO;
+	[view startAnimation];
+	AudioSessionSetActive(true);
+}
+
+- (void)applicationWillResignActive:(UIApplication *)application {
+	//stop animation before going into background
+    view.applicationResignedActive = YES;
+    [view stopAnimation];
+}
+
+- (void)applicationDidEnterBackground:(UIApplication *)application {
+}
+
+- (void)applicationWillEnterForeground:(UIApplication *)application {
+}
+
+
+- (void)dealloc
+{	
+	delete[] dcFilter;
+	
+	[view release];
+	[window release];
+	
+	free(oscilLine);
+
+	[super dealloc];
+}
+
+
+- (void)createGLTexture:(GLuint *)texName fromCGImage:(CGImageRef)img
+{
+	GLubyte *spriteData = NULL;
+	CGContextRef spriteContext;
+	GLuint imgW, imgH, texW, texH;
+	
+	imgW = CGImageGetWidth(img);
+	imgH = CGImageGetHeight(img);
+	
+	// Find smallest possible powers of 2 for our texture dimensions
+	for (texW = 1; texW < imgW; texW *= 2) ;
+	for (texH = 1; texH < imgH; texH *= 2) ;
+	
+	// Allocated memory needed for the bitmap context
+	spriteData = (GLubyte *) calloc(texH, texW * 4);
+	// Uses the bitmatp creation function provided by the Core Graphics framework. 
+	spriteContext = CGBitmapContextCreate(spriteData, texW, texH, 8, texW * 4, CGImageGetColorSpace(img), kCGImageAlphaPremultipliedLast);
+	
+	// Translate and scale the context to draw the image upside-down (conflict in flipped-ness between GL textures and CG contexts)
+	CGContextTranslateCTM(spriteContext, 0., texH);
+	CGContextScaleCTM(spriteContext, 1., -1.);
+	
+	// After you create the context, you can draw the sprite image to the context.
+	CGContextDrawImage(spriteContext, CGRectMake(0.0, 0.0, imgW, imgH), img);
+	// You don't need the context at this point, so you need to release it to avoid memory leaks.
+	CGContextRelease(spriteContext);
+	
+	// Use OpenGL ES to generate a name for the texture.
+	glGenTextures(1, texName);
+	// Bind the texture name. 
+	glBindTexture(GL_TEXTURE_2D, *texName);
+	// Speidfy a 2D texture image, provideing the a pointer to the image data in memory
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, texW, texH, 0, GL_RGBA, GL_UNSIGNED_BYTE, spriteData);
+	// Set the texture parameters to use a minifying filter and a linear filer (weighted average)
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	
+	// Enable use of the texture
+	glEnable(GL_TEXTURE_2D);
+	// Set a blending function to use
+	glBlendFunc(GL_SRC_ALPHA,GL_ONE);
+	//glBlendFunc(GL_ONE, GL_ONE_MINUS_SRC_ALPHA);
+	// Enable blending
+	glEnable(GL_BLEND);
+	
+	free(spriteData);
+}
+
+- (void)clearTextures
+{
+	bzero(texBitBuffer, sizeof(UInt32) * 512);
+}
+
+
+- (void)drawOscilloscope
+{
+	// Clear the view
+	glClear(GL_COLOR_BUFFER_BIT);
+	
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE);
+	
+	glColor4f(1., 1., 1., 1.);
+	
+	glPushMatrix();
+	
+	glTranslatef(0., 480., 0.);
+	glRotatef(-90., 0., 0., 1.);
+	
+	
+	glEnable(GL_TEXTURE_2D);
+	glEnableClientState(GL_VERTEX_ARRAY);
+	glEnableClientState(GL_TEXTURE_COORD_ARRAY);
+	
+	{
+		// Draw our background oscilloscope screen
+		const GLfloat vertices[] = {
+			0., 0.,
+			512., 0., 
+			0.,  512.,
+			512.,  512.,
+		};
+		const GLshort texCoords[] = {
+			0, 0,
+			1, 0,
+			0, 1,
+			1, 1,
+		};
+		
+		glVertexPointer(2, GL_FLOAT, 0, vertices);
+		glTexCoordPointer(2, GL_SHORT, 0, texCoords);
+		
+		glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
+	}
+
+	GLfloat *oscilLine_ptr;
+	GLfloat max = drawBufferLen;
+	SInt8 *drawBuffer_ptr;
+	
+	// Alloc an array for our oscilloscope line vertices
+	if (resetOscilLine) {
+		oscilLine = (GLfloat*)realloc(oscilLine, drawBufferLen * 2 * sizeof(GLfloat));
+		resetOscilLine = NO;
+	}
+	
+	glPushMatrix();
+	
+	// Translate to the left side and vertical center of the screen, and scale so that the screen coordinates
+	// go from 0 to 1 along the X, and -1 to 1 along the Y
+	glTranslatef(17., 182., 0.);
+	glScalef(448., 116., 1.);
+	
+	// Set up some GL state for our oscilloscope lines
+	glDisable(GL_TEXTURE_2D);
+	glDisableClientState(GL_TEXTURE_COORD_ARRAY);
+	glDisableClientState(GL_COLOR_ARRAY);
+	glDisable(GL_LINE_SMOOTH);
+	glLineWidth(2.);
+	
+	int drawBuffer_i;
+	// Draw a line for each stored line in our buffer (the lines are stored and fade over time)
+	for (drawBuffer_i=0; drawBuffer_i<kNumDrawBuffers; drawBuffer_i++)
+	{
+		if (!drawBuffers[drawBuffer_i]) continue;
+		
+		oscilLine_ptr = oscilLine;
+		drawBuffer_ptr = drawBuffers[drawBuffer_i];
+		
+		GLfloat i;
+		// Fill our vertex array with points
+		for (i=0.; i<max; i=i+1.)
+		{
+			*oscilLine_ptr++ = i/max;
+			*oscilLine_ptr++ = (Float32)(*drawBuffer_ptr++) / 128.;
+		}
+		
+		// If we're drawing the newest line, draw it in solid green. Otherwise, draw it in a faded green.
+		if (drawBuffer_i == 0)
+			glColor4f(0., 1., 0., 1.);
+		else
+			glColor4f(0., 1., 0., (.24 * (1. - ((GLfloat)drawBuffer_i / (GLfloat)kNumDrawBuffers))));
+		
+		// Set up vertex pointer,
+		glVertexPointer(2, GL_FLOAT, 0, oscilLine);
+		
+		// and draw the line.
+		glDrawArrays(GL_LINE_STRIP, 0, drawBufferLen);
+		
+	}
+	
+	glPopMatrix();
+		
+	glPopMatrix();
+}
+
+- (void)drawView:(id)sender forTime:(NSTimeInterval)time
+{
+    [self drawOscilloscope];
+}
+
+- (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event
+{
+	
+}
+
+- (void)touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event
+{
+	
+}
+
+- (void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event
+{
+
+}
+
+@end
